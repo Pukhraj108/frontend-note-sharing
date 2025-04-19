09dff4870be120dcd9d7b78648c37ba4df248e47
gitandgithub <agentpdhami@gmail.com>
2025-04-19 15:22:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import AppRouter from './router'
+import './App.css'
+import 'animate.css'
+
+function App() {
+  
+
+  return(
+    <div>
+     <AppRouter/>
+    </div>
+  )
+
+}
+
+export default App
