6d9c5c387ae45e5a229bb97b528b39a7c4d1d5a2
gitandgithub <agentpdhami@gmail.com>
2025-04-21 14:57:16 +0530

    Initial commit

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
-import StudentLoginPage from './pages/studentLoginpage';
+import StudentLoginPage from './pages/StudentLoginpage';
 import EducatorLoginPage from './pages/EducatorLoginPage';
 
 
